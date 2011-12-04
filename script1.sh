58974072159966fbf1fd6e4640e5ae70393ea25a
pgn <pgn@ubuntu.ubuntu-domain>
2011-12-04 15:47:22 +0000

    Initial commit for script

---
+++ script1.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+clear
+
+echo "Hello $USER"
+
+echo
+
+echo "Listing current users"
+
+w
+
+COLOUR=black
+Value=9
+
+echo "Setting up the colour = $COLOUR"
+echo "Value $Value"
+echo
+
+echo "returning control back"
